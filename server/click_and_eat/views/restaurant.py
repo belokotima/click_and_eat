87c33e5ef784b25511667a0d7e25d2bf4cc4b150
ilya_sh <i.a.shishov@gmail.com>
2020-05-14 18:42:38 +0300

    implemented cart

--- server/click_and_eat/views/restaurant.py
+++ server/click_and_eat/views/restaurant.py
@@ -1,7 +1,90 @@
 from ..forms import *
 from .base_views import *
+import json
 from django.views import View
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
+
+
+class CartProduct:
+    def __init__(self, product_id, name, count):
+        self.product_id = product_id
+        self.name = name
+        self.count = count
+
+    @classmethod
+    def from_json(cls, json_string):
+        json_dict = json.loads(json_string)
+        return cls(**json_dict)
+
+    @classmethod
+    def from_dict(cls, dictionary):
+        return cls(**dictionary)
+
+    def to_dict(self):
+        return self.__dict__
+
+
+class Cart:
+    def __init__(self, products, **kwargs):
+        self.products = []
+
+        for product in products:
+            self.products.append(CartProduct.from_dict(product))
+
+    def find(self, product_id):
+        products = list(filter(lambda x: x.product_id == product_id, self.products))
+
+        if len(products) >= 1:
+            return products[0]
+        else:
+            return None
+
+    def append(self, product_id, name, count=1):
+        product = self.find(product_id)
+        if product is None:
+            product = CartProduct(product_id, name, count)
+            self.products.append(product)
+        else:
+            product.name = name
+            product.count = product.count + count
+
+        if product.count <= 0:
+            self.products.remove(product)
+
+    def remove(self, product_id):
+        product = self.find(product_id)
+        if product is not None:
+            self.products.remove(product)
+
+    def clear(self):
+        self.products = []
+
+    @classmethod
+    def load(cls, request):
+        cart = request.session.get('cart', Cart([]).to_dict())
+        return Cart.from_dict(cart)
+
+    def save(self, request):
+        request.session['cart'] = self.to_dict()
+
+    @classmethod
+    def from_json(cls, json_string):
+        json_dict = json.loads(json_string)
+        return cls(**json_dict)
+
+    @classmethod
+    def from_dict(cls, dictionary):
+        return cls(**dictionary)
+
+    def to_dict(self):
+        products = []
+        products_ids = []
+
+        for product in self.products:
+            products_ids.append(product.product_id)
+            products.append(product.to_dict())
+
+        return {'products_ids': products_ids, 'products': products}
 
 
 class RestaurantView(View):
@@ -13,13 +96,53 @@ class RestaurantView(View):
         return render(request, self.template_name, context)
 
 
-class RestaurantAddToCart(View):
+class CartViewComponent(View):
+    template_name = 'templates/base/cart.html'
 
-    def get(self, request, restaurant_id, product_id, *args, **kwargs):
-        cart = request.session.get('cart', [])
-        product = get_object_or_404(Product, restaurant_id=restaurant_id, id=product_id)
-        product_context = {'name': product.name}
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
 
-        cart.append(product_context)
-        request.session['cart'] = cart
-        return redirect('restaurant', restaurant_id=restaurant_id)
+
+class CartAdd(View):
+
+    def get(self, request, product_id, count, *args, **kwargs):
+        cart = Cart.load(request)
+
+        product = get_object_or_404(Product, id=product_id)
+
+        cart.append(product.id, product.name, count)
+        cart.save(request)
+        return HttpResponse('OK')
+
+
+class CartDelete(View):
+
+    def get(self, request, product_id, count,*args, **kwargs):
+        cart = Cart.load(request)
+
+        product = get_object_or_404(Product, id=product_id)
+
+        cart.append(product.id, product.name, -count)
+        cart.save(request)
+        return HttpResponse('OK')
+
+
+class CartRemove(View):
+
+    def get(self, request, product_id, *args, **kwargs):
+        cart = Cart.load(request)
+
+        product = get_object_or_404(Product, id=product_id)
+
+        cart.remove(product.id)
+        cart.save(request)
+        return HttpResponse('OK')
+
+class CartClear(View):
+
+    def get(self, request):
+        cart = Cart.load(request)
+        cart.clear()
+        cart.save(request)
+        return HttpResponse('OK')
+
